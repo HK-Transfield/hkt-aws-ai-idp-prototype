58af5b9dae4dbca57d83e99c57415ec7441c78b9
HK Transfield <h.k.transfield1@gmail.com>
2025-02-18 16:31:06 +1300

    add header comments

--- modules/lambda-function-builder/output.tf
+++ modules/lambda-function-builder/output.tf
@@ -1,3 +1,8 @@
+/*
+Name:     Lambda Function Builder Module
+Project:  AWS Generative AI Backed IDP Solution
+Author:   HK Transfield, 2024
+*/
 
 output "lambda_function_arn" {
   description = "ARN of the Lambda function"
