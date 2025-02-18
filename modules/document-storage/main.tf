6370bf6cc5a3df7cff10e7e9d3579740a8628d3f
HK Transfield <h.k.transfield1@gmail.com>
2025-02-18 16:25:15 +1300

    add lifecycle and server-side encryption to Bucket

--- modules/document-storage/main.tf
+++ modules/document-storage/main.tf
@@ -1,13 +1,18 @@
 /*
-Name: Document Storage Module
-Author: HK Transfield, 2024
+Name:     Document Storage Module
+Project:  AWS Generative AI Backed IDP Solution
+Author:   HK Transfield, 2024
 
-A simple module that creates an S3 bucket with the specified settings.
-The intended use within this project is to store raw documents, classified
-documents sorted by prefix, and the extracted data enriched douments.
+A module for configuring an S3 bucket with the appropriate ACL settings.
+It also follows best security and compliance practices by enabling bucket 
+ownership controls, enabling versioning, and defining lifecycle policies.
 */
 
-# To ensure that the bucket name is unique, we can append a random string to it
+################################################################################
+# General configuration
+################################################################################
+
+# Append a randomly generated string to the bucket name to ensure uniqueness
 resource "random_string" "this" {
   length  = 16
   special = false
@@ -16,8 +21,19 @@ resource "random_string" "this" {
 
 resource "aws_s3_bucket" "this" {
   bucket        = "${var.bucket_name}-${random_string.this.result}"
-  force_destroy = var.force_destroy
+  force_destroy = var.force_destroy # DON'T DO THIS IN PRODUCTION
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.bucket_name
+    },
+  )
 }
+
+################################################################################
+# Security and compliance configuration
+################################################################################
 
 resource "aws_s3_bucket_ownership_controls" "this" {
   bucket = aws_s3_bucket.this.id
@@ -32,4 +48,35 @@ resource "aws_s3_bucket_acl" "this" {
 
   bucket = aws_s3_bucket.this.id
   acl    = var.acl
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.versioning_status
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    id     = "abort-incomplete-multipart-upload"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = var.noncurrent_version_expiration_days
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
