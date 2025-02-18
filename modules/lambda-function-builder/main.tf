657333d71f7ffab19f44ccdef13951bc6b9f4f3f
HK Transfield <h.k.transfield1@gmail.com>
2025-02-18 16:31:21 +1300

    add tags to resources

--- modules/lambda-function-builder/main.tf
+++ modules/lambda-function-builder/main.tf
@@ -1,23 +1,16 @@
 /*
-Name: Lambda Function Builder
-Author: HK Transifeld, 2024
+Name:     Lambda Function Builder Module
+Project:  AWS Generative AI Backed IDP Solution
+Author:   HK Transfield, 2024
 
-This module configures the resources needed to set up 
-any Lambda functions with the intended policies and
-settings.
-
-This module is intended to be used as a building block
-for functions that perform the following tasks:
-  - Document processing
-  - Document classification
-  - Document enrichment and extraction
-  - Review and validation automation
+This module acts as a building block for configuring Lambda functions, along 
+with their associated IAM roles and policies.
 */
 
 data "aws_region" "current" {}
 
 ################################################################################
-# IAM Roles and Policy attachment for Lambda
+# IAM role and policy configuration
 ################################################################################
 
 resource "aws_iam_role" "this" {
@@ -33,11 +26,25 @@ resource "aws_iam_role" "this" {
       }
     ]
   })
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.iam_role_name
+    },
+  )
 }
 
 resource "aws_iam_policy" "this" {
   name   = var.iam_policy_name
   policy = var.iam_policy_json
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.iam_policy_name
+    },
+  )
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
@@ -55,7 +62,8 @@ locals {
   lambda_handler = "${var.lambda_filename}.lambda_handler"
 }
 
-data "archive_file" "this" {
+# locally archive the lambda source code
+data "archive_file" "local" {
   type        = "zip"
   source_file = local.lambda_source
   output_path = local.lambda_output
@@ -72,5 +80,75 @@ resource "aws_lambda_function" "this" {
 
   environment {
     variables = var.environment_variables
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.lambda_filename
+    },
+  )
+
+  tracing_config {
+    mode = "Active"
+  }
+
+  publish    = true
+  depends_on = [aws_cloudwatch_log_group.this]
+}
+
+resource "aws_lambda_alias" "this" {
+  name             = "latest"
+  function_name    = aws_lambda_function.this.function_name
+  function_version = "$LATEST"
+}
+
+################################################################################
+# CloudWatch logging and monitoring configuration
+################################################################################
+
+locals {
+  cloudwatch_log_group_name    = "${var.lambda_filename}-log-group"
+  cloudwatch_metric_alarm_name = "${var.lambda_filename}-error-metric-alarm"
+  sns_topic_name               = "${var.lambda_filename}-error-topic"
+}
+
+resource "aws_sns_topic" "lambda_error" {
+  name = local.sns_topic_name
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = local.sns_topic_name
+    },
+  )
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/lambda/${aws_lambda_function.this.function_name}"
+  retention_in_days = var.cloudwatch_log_retention_in_days
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = local.cloudwatch_log_group_name
+    },
+  )
+}
+
+resource "aws_cloudwatch_metric_alarm" "this" {
+  alarm_name          = local.cloudwatch_metric_alarm_name
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_description   = "Triggers when the ${var.lambda_filename} Lambda function has errors."
+  alarm_actions       = [aws_sns_topic.lambda_error.arn]
+
+  dimensions = {
+    FunctionName = aws_lambda_function.this.function_name
   }
 }
