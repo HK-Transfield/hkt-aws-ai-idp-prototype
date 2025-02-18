4c7d71e66d2dd7053bb499331e231277c6f32f93
HK Transfield <h.k.transfield1@gmail.com>
2025-02-18 16:25:42 +1300

    add appropriate comments

--- modules/textract-event-handler/variables.tf
+++ modules/textract-event-handler/variables.tf
@@ -1,7 +1,25 @@
+/*
+Name: Event Handler Module
+Project:  AWS Generative AI Backed IDP Solution
+Author: HK Transifeld, 2024
+*/
+
 variable "sns_topic_name" {
-  type = string
+  type        = string
+  description = "The SNS topic name"
 }
 
 variable "sqs_queue_name" {
-  type = string
+  type        = string
+  description = "The SQS queue name"
 }
+
+################################################################################
+# Other configurations
+################################################################################
+
+variable "tags" {
+  type        = map(string)
+  description = "A mapping of tags to assign to the resource"
+}
+
