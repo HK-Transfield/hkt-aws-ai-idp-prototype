d9c642ee359609183279870b7f871351a95e9852
HK Transfield <h.k.transfield1@gmail.com>
2025-02-18 16:34:21 +1300

    add variables for lifecycle and versioning

--- modules/document-storage/variables.tf
+++ modules/document-storage/variables.tf
@@ -1,3 +1,13 @@
+/*
+Name:     Document Storage Module
+Project:  AWS Generative AI Backed IDP Solution
+Author:   HK Transfield, 2024
+*/
+
+################################################################################
+# Bucket configurations
+################################################################################
+
 variable "bucket_name" {
   description = "Name of the bucket"
   type        = string
@@ -18,5 +28,27 @@ variable "acl" {
 variable "force_destroy" {
   description = "Whether to allow force destroying the bucket"
   type        = bool
-  default     = false
+  default     = true # this is just for demo purposes and should be set to false in production
+}
+
+variable "versioning_status" {
+  type        = string
+  default     = "Enabled"
+  description = "The versioning status for the bucket"
+}
+
+variable "noncurrent_version_expiration_days" {
+  type        = number
+  default     = 30
+  description = "The number of days to keep noncurrent versions"
+
+}
+
+################################################################################
+# Other configurations
+################################################################################
+
+variable "tags" {
+  type        = map(string)
+  description = "A mapping of tags to assign to the resource"
 }
