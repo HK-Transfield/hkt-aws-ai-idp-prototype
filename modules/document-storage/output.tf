3be239f5a7168e1f87f52f2f61e559514c110aa2
HK Transfield <h.k.transfield1@gmail.com>
2025-02-18 16:28:41 +1300

    add outputs for domain name and bucket name

--- modules/document-storage/output.tf
+++ modules/document-storage/output.tf
@@ -1,5 +1,14 @@
 output "bucket_arn" {
   description = "The ARN of the bucket"
   value       = aws_s3_bucket.this.arn
+}
 
+output "bucket_domain_name" {
+  description = "Value of the bucket domain name"
+  value       = aws_s3_bucket.this.bucket_domain_name
+}
+
+output "bucket_id" {
+  description = "The name of the bucket"
+  value       = aws_s3_bucket.this.id
 }
