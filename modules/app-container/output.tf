4c364d570daee3591b553b0396f82241fd691c4c
HK Transfield <hktransfield.dev@gmail.com>
2025-03-06 16:34:58 +1300

    fix: update resource name

--- modules/app-container/output.tf
+++ modules/app-container/output.tf
@@ -1,19 +1,14 @@
-output "alb_dns_name" {
-  description = "DNS name of the ALB"
-  value       = aws_lb.streamlit.dns_name
-}
-
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
-  value       = aws_ecs_cluster.streamlit.name
+  value       = aws_ecs_cluster.this.name
 }
 
 output "ecs_service_name" {
   description = "Name of the ECS service"
-  value       = aws_ecs_service.streamlit.name
+  value       = aws_ecs_service.this.name
 }
 
 output "cloudwatch_log_group" {
   description = "Name of the CloudWatch log group"
-  value       = aws_cloudwatch_log_group.streamlit.name
+  value       = aws_cloudwatch_log_group.this.name
 }
