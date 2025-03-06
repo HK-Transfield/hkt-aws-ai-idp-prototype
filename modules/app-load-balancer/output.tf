11ff3ae7e412f9409ab65119afd2cc6ecc78967f
HK Transfield <hktransfield.dev@gmail.com>
2025-03-06 16:41:18 +1300

    style: update resource values to 'this'

--- modules/app-load-balancer/output.tf
+++ modules/app-load-balancer/output.tf
@@ -1,14 +1,14 @@
 output "alb_dns_name" {
-  value       = aws_lb.main.dns_name
+  value       = aws_lb.this.dns_name
   description = "DNS name of the load balancer"
 }
 
 output "target_group_arn" {
-  value       = aws_lb_target_group.app.arn
+  value       = aws_lb_target_group.this.arn
   description = "ARN of the target group"
 }
 
 output "alb_security_group_id" {
-  value       = aws_security_group.alb.id
+  value       = aws_security_group.this.id
   description = "Security group ID for the ALB"
 }
