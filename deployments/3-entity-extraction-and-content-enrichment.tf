20bf51c4b3ff5bd9e7a63a43f44eb675ea633f73
HK Transfield <hktransfield.dev@gmail.com>
2025-03-06 16:28:10 +1300

    docs: add header comment and move bucket

--- deployments/3-entity-extraction-and-content-enrichment.tf
+++ deployments/3-entity-extraction-and-content-enrichment.tf
@@ -1,5 +1,15 @@
+/*
+Name:     IDP Entity Extraction and Content Enrichment
+Project:  AWS Generative AI Backed IDP Solution
+Author:   HK Transfield, 2025
+
+This configuration is the third stage of the IDP pipeline. Once data has been
+captured and extracted, it needs to be categorised into specific classes before
+further classification can be performed.
+*/
+
 ################################################################################
-# ENTITY EXTRACTION AND CONTENT ENRICHMENT
+# LAMBDA FUNCTION FOR ENTITY EXTRACTION AND CONTENT ENRICHMENT
 ################################################################################
 
 # allow lambda access to the SQS, Textract, and Bedrock API
@@ -33,7 +43,7 @@ module "entity_extraction_and_content_enrichment_lambda_function" {
   cloudwatch_log_retention_in_days = 7
 
   environment_variables = {
-    TARGET_BUCKET           = aws_s3_bucket.enriched_documents_bucket.bucket
+    TARGET_BUCKET           = module.extracted_data_and_enriched_documents_bucket.bucket_id
     CLASSIFIED_DOCS_OBJ_KEY = local.classified_documents_object_key
   }
 
@@ -44,5 +54,21 @@ module "entity_extraction_and_content_enrichment_lambda_function" {
   tags = {
     project = local.project_tag
     name    = "3-entity-extraction-and-content-enrichment"
+  }
+}
+
+################################################################################
+# EXTRACTION AND ENRICHMENT DOCUMENT STORAGE
+################################################################################
+
+module "extracted_data_and_enriched_documents_bucket" {
+  source = "../modules/document-storage"
+
+  bucket_name   = local.validated_documents_bucket_name
+  force_destroy = true
+
+  tags = {
+    project = local.project_tag
+    name    = local.validated_documents_bucket_name
   }
 }
