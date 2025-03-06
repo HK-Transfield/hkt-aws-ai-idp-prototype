e257e6e609232ecc8d32fdeb7d8eb2e052f1e375
HK Transfield <hktransfield.dev@gmail.com>
2025-03-06 16:42:18 +1300

    style: remove header comments and update names to private or public

--- modules/app-network/outputs.tf
+++ modules/app-network/outputs.tf
@@ -1,39 +1,19 @@
-################################################################################
-# VPC
-################################################################################
-
 output "vpc_id" {
   value = aws_vpc.this.id
 }
 
-################################################################################
-# Public Subnets
-################################################################################
-
-output "web_a_subnet_id" {
-  value = aws_subnet.web["A"].id
+output "public_a_subnet_id" {
+  value = aws_subnet.public["A"].id
 }
 
-output "web_b_subnet_id" {
-  value = aws_subnet.web["B"].id
+output "public_b_subnet_id" {
+  value = aws_subnet.public["B"].id
 }
 
-################################################################################
-# Private Subnets
-################################################################################
-
-output "app_a_subnet_id" {
-  value = aws_subnet.app["A"].id
+output "private_a_subnet_id" {
+  value = aws_subnet.private["A"].id
 }
 
-output "app_b_subnet_id" {
-  value = aws_subnet.app["B"].id
-}
-
-output "db_a_subnet_id" {
-  value = aws_subnet.db["A"].id
-}
-
-output "db_b_subnet_id" {
-  value = aws_subnet.db["B"].id
+output "private_b_subnet_id" {
+  value = aws_subnet.private["B"].id
 }
