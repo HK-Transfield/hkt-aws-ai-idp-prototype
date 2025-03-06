ab448925824adacb1b67a8efae25091b10b79a13
HK Transfield <hktransfield.dev@gmail.com>
2025-03-06 16:40:10 +1300

    style: rename to function_arn

--- modules/lambda-function-builder/output.tf
+++ modules/lambda-function-builder/output.tf
@@ -4,7 +4,7 @@ Project:  AWS Generative AI Backed IDP Solution
 Author:   HK Transfield, 2024
 */
 
-output "lambda_function_arn" {
+output "function_arn" {
   description = "ARN of the Lambda function"
   value       = aws_lambda_function.this.arn
 }
